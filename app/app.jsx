3c7171c7f8e6c89f5c0630a786841c7953c1e597
mjdude <mo@syncspot.net>
2016-05-25 17:04:54 -0700

    updated with babel stage 0 plugin for additional es6 features

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objectOne = {
+  name: 'Mo',
+  location: 'San Francisco'
+};
+
+var objectTwo = {
+  age: 24,
+  ...objectOne
+};
+
+console.log(objectTwo);
 ReactDOM.render(
   <h1>Boilerplate app</h1>,
   document.getElementById('app')
